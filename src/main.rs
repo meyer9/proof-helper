1ae82bdfb91909ecec4bf416442a8a3f54c3f6eb
Julian Meyer <julian.meyer@coinbase.com>
2025-07-08 14:47:51 +0000

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
 use futures_util::TryStreamExt;
+use op_reth::node::OpNode;
+use op_reth::primitives::OpPrimitives;
 use reth::{
-    api::FullNodeComponents, 
-    builder::NodeTypes, 
-    primitives::{EthPrimitives, SealedBlock}, 
-    providers::{StateReader}, 
+    api::{ConfigureEvm, FullNodeComponents, NodePrimitives}, builder::NodeTypes, core::primitives::AlloyBlockHeader, primitives::{RecoveredBlock}, providers::{StateProviderFactory, StateReader}, revm::{database::StateProviderDatabase, witness::ExecutionWitnessRecord, State}
 };
-use reth_execution_types::Chain;
 use reth_exex::{ExExContext, ExExEvent, ExExNotification};
-use reth_node_ethereum::EthereumNode;
-use reth_trie_db::{MerklePatriciaTrie};
-use reth_tracing::tracing::info;
+use reth_tracing::tracing::{info, warn};
+use reth_trie_db::MerklePatriciaTrie;
+use reth_evm::execute::Executor;
 
 /// Proof Helper ExEx - processes blocks and tracks state changes
-pub struct ProofHelper<Node> 
-where 
+pub struct ProofHelper<Node>
+where
     Node: FullNodeComponents<
-        Types: NodeTypes<Primitives = EthPrimitives, StateCommitment = MerklePatriciaTrie>
+        Types: NodeTypes<Primitives = OpPrimitives, StateCommitment = MerklePatriciaTrie>,
     >,
     Node::Provider: StateReader,
 {
@@ -25,82 +23,77 @@ where
 impl<Node> ProofHelper<Node>
 where
     Node: FullNodeComponents<
-        Types: NodeTypes<Primitives = EthPrimitives, StateCommitment = MerklePatriciaTrie>
+        Types: NodeTypes<Primitives = OpPrimitives, StateCommitment = MerklePatriciaTrie>,
     >,
-    Node::Provider: StateReader,
+    Node::Provider: StateReader + StateProviderFactory,
 {
     /// Create a new ProofHelper instance
     pub fn new(ctx: ExExContext<Node>) -> Self {
         Self { ctx }
     }
 
-    /// Process a single block and extract state changes
-    async fn process_block(&self, block: &SealedBlock) -> eyre::Result<()> {
-        let block_number = block.number;
-        let block_hash = block.hash();
-        
-        info!(
-            block_number = %block_number,
-            block_hash = %block_hash,
-            "Processing block"
-        );
 
-        // Get the execution outcome for this block
-        if let Ok(Some(execution_outcome)) = self.ctx.components.provider().get_state(block_number) {
-            // Process account changes from reverts
-            for (address, account) in execution_outcome.bundle.reverts.iter().flatten() {
-                info!(
-                    address = %address,
-                    account = ?account,
-                    "State change detected"
-                );
-            }
-        }
-
-        Ok(())
-    }
-
-    /// Process a chain of committed blocks
-    async fn process_chain(&self, chain: &Chain) -> eyre::Result<()> {
-        info!(
-            chain_range = ?chain.range(),
-            "Processing committed chain"
-        );
-
-        // Process each block in the chain
-        for (_, block) in chain.blocks().iter() {
-            self.process_block(block).await?;
-        }
-
+    fn process_block(
+        &self,
+        block: &RecoveredBlock<<<Node::Types as NodeTypes>::Primitives as NodePrimitives>::Block>,
+    ) -> eyre::Result<()> {
+        let state_provider = self.ctx.provider().state_by_block_id(block.header().number().saturating_sub(1).into())?;
+    
+        let db = StateProviderDatabase::new(&state_provider);
+        let block_executor = self.ctx.evm_config().batch_executor(db);
+    
+        let mut witness_record = ExecutionWitnessRecord::default();
+    
+        let _ = block_executor
+            .execute_with_state_closure(&(*block).clone(), |statedb: &State<_>| {
+                witness_record.record_executed_state(statedb);
+            })
+            .map_err(|err| eyre::eyre!(err))?;
+    
+        let ExecutionWitnessRecord {
+            hashed_state,
+            codes,
+            keys,
+            ..
+        } = witness_record;
+    
+        let state = state_provider
+            .witness(Default::default(), hashed_state)
+            .map_err(|err| eyre::eyre!(err))?;
+    
+        info!("Got {:?} state nodes, {:?} code nodes, {:?} key nodes", state.len(), codes.len(), keys.len());
+    
         Ok(())
     }
 
     /// Main execution loop for the ExEx
     pub async fn run(mut self) -> eyre::Result<()> {
+        const MAX_BLOCK_DIFF: u64 = 1000;
+
         while let Some(notification) = self.ctx.notifications.try_next().await? {
             match &notification {
                 ExExNotification::ChainCommitted { new } => {
-                    info!(committed_chain = ?new.range(), "Received commit");
-                    self.process_chain(new).await?;
+                    let head_block_number = new.tip().num_hash().number;
+
+                    for (block_number, block) in new.blocks() {
+                        if head_block_number.saturating_sub(*block_number) > MAX_BLOCK_DIFF {
+                            warn!("Block {} is too far behind the head block {}, skipping", block_number, head_block_number);
+                            continue;
+                        }
+
+                        if let Err(err) = self.process_block(block) {
+                            warn!("Error processing block {}: {}", block_number, err);
+                        }
+                    }
                 }
-                ExExNotification::ChainReorged { old, new } => {
-                    info!(
-                        from_chain = ?old.range(), 
-                        to_chain = ?new.range(), 
-                        "Received reorg"
-                    );
-                    // Process the new chain after reorg
-                    self.process_chain(new).await?;
-                }
-                ExExNotification::ChainReverted { old } => {
-                    info!(reverted_chain = ?old.range(), "Received revert");
-                    // Handle reverted blocks if needed
-                }
+                _ => {}
             };
 
             // Send finish event for committed chain
             if let Some(committed_chain) = notification.committed_chain() {
-                self.ctx.events.send(ExExEvent::FinishedHeight(committed_chain.tip().num_hash()))?;
+                self.ctx
+                    .events
+                    .send(ExExEvent::FinishedHeight(committed_chain.tip().num_hash()))?;
             }
         }
 
@@ -109,9 +102,9 @@ where
 }
 
 fn main() -> eyre::Result<()> {
-    reth::cli::Cli::parse_args().run(async move |builder, _| {
+    op_reth::cli::Cli::parse_args().run(async move |builder, _| {
         let handle = builder
-            .node(EthereumNode::default())
+            .node(OpNode::default())
             .install_exex("proof-helper", async move |ctx| {
                 let proof_helper = ProofHelper::new(ctx);
                 Ok(proof_helper.run())
